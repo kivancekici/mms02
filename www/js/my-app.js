d3952230f964c02a7a9bf2df4d6344083516fa1d
Aşkın Sağlam <saglamaskin@gmail.com>
2018-04-29 01:59:08 +0300

    sadasd

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -350,13 +350,14 @@ $$(document).on('pageInit', function(e) {
 
 
                 toDataURL('./images/indir.bmp', function(dataUrl) {
+                    dataURL.replace(/^data:image\/(x-ms-bmp|jpg);base64,/, "");
                     myApp.alert(dataUrl);
                 });
 
                 if (bool == true) {
-
+                    myApp.alert('hello');
                     toDataURL('./images/indir.bmp', function(dataUrl) {
-
+                        dataURL.replace(/^data:image\/(x-ms-bmp|jpg);base64,/, "");
                         cordova.plugins.brotherPrinter.printViaSDK(dataUrl, function(error) {
                             myApp.alert(error);
                         });
